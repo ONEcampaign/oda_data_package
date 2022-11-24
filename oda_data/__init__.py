933e8102f66bb64270422e60333de92ba0314e8b
Jorge Rivera <jorge.rivera@one.org>
2022-11-24 18:04:45 +0100

    paths changes

--- oda_data/__init__.py
+++ oda_data/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 from oda_data.classes.oda_data import ODAData
 from oda_data.get_data.crs import download_crs
 from oda_data.get_data.dac1 import download_dac1
@@ -6,7 +6,7 @@ from oda_data.get_data.dac2a import download_dac2a
 from oda_data.get_data.multisystem import download_multisystem
 
 
-def set_pydeflate_path(path):
+def set_data_path(path):
     from pathlib import Path
     from oda_data.config import OdaPATHS
 
