f610b986aad99fc15fb791949ef03be434962f61
Jorge Rivera <jorge.rivera@one.org>
2022-11-24 17:37:33 +0100

    Update config.py

--- oda_data/config.py
+++ oda_data/config.py
@@ -5,8 +5,8 @@ class OdaPATHS:
     """Class to store the paths to the data and output folders."""
 
     project = Path(__file__).resolve().parent.parent
-    scripts = project / "scripts"
-    raw_data = scripts / "raw_data"
+    scripts = project / "oda_data"
+    raw_data = scripts / ".raw_data"
     pydeflate = raw_data / ".pydeflate"
     logs = scripts / ".logs"
     indicators = scripts / "indicators"
