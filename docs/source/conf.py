aefa36959b217d4f24057572a358b1967f48c9a7
Jorge Rivera <jorge.rivera@one.org>
2022-11-25 12:45:06 +0100

    release

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,26 +9,25 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../../oda_data/'))
+sys.path.insert(0, os.path.abspath("../../oda_data/"))
 
-project = 'oda_data'
-copyright = '2022, Jorge Rivera'
-author = 'Jorge Rivera'
-release = '0.1.5'
+project = "oda_data"
+copyright = "2022, Jorge Rivera"
+author = "Jorge Rivera"
+release = "0.1.5"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'myst_parser']
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "myst_parser"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
-source_suffix = {'.md': 'markdown',
-                 '.rst': 'restructuredtext'}
+source_suffix = {".md": "markdown", ".rst": "restructuredtext"}
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
