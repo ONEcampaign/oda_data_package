8a580b213adda66d64c6c734cb376ccf13fbccfe
Jorge Rivera <jorge.rivera@one.org>
2022-12-09 17:56:00 +0100

    Bump version: 0.1.9 → 0.1.10

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.abspath("../../oda_data/"))
 project = "oda_data"
 copyright = "2022, Jorge Rivera"
 author = "Jorge Rivera"
-release = "0.1.9"
+release = "0.1.10"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
--- tests/test_oda_data.py
+++ tests/test_oda_data.py
@@ -2,5 +2,5 @@ from oda_data import __version__
 
 
 def test_version():
-    current_version = "0.1.9"
+    current_version = "0.1.10"
     assert __version__ == current_version
