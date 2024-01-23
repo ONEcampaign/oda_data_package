48c46a59ca13fe21a6678e6d383bc65757d726f7
Jorge Rivera <jorge.rivera@one.org>
2024-01-23 17:54:18 +0100

    fix dtypes

--- oda_data/clean_data/dtypes.py
+++ oda_data/clean_data/dtypes.py
@@ -29,12 +29,13 @@ def schema_types() -> dict:
         OdaSchema.RECIPIENT_REGION: "string[pyarrow]",
         OdaSchema.RECIPIENT_REGION_CODE: "int16[pyarrow]",
         OdaSchema.RECIPIENT_INCOME: "string[pyarrow]",
+        OdaSchema.RECIPIENT_INCOME_CODE: "int16[pyarrow]",
         OdaSchema.FLOW_MODALITY: "string[pyarrow]",
         OdaSchema.ALLOCABLE_SHARE: "float64[pyarrow]",
         OdaSchema.CONCESSIONALITY: "string[pyarrow]",
         OdaSchema.FINANCIAL_INSTRUMENT: "string[pyarrow]",
         OdaSchema.FLOW_TYPE: "string[pyarrow]",
-        OdaSchema.FINANCE_TYPE: "string[pyarrow]",
+        OdaSchema.FINANCE_TYPE: "int16[pyarrow]",
         OdaSchema.CHANNEL_NAME_DELIVERY: "string[pyarrow]",
         OdaSchema.CHANNEL_CODE_DELIVERY: "int32[pyarrow]",
         OdaSchema.CHANNEL_CODE: "int32[pyarrow]",
@@ -67,6 +68,7 @@ def schema_types() -> dict:
         OdaSchema.CLIMATE_SHARE_ROLLING: "float64[pyarrow]",
         OdaSchema.FLOW_CODE: "int32[pyarrow]",
         OdaSchema.FLOW_NAME: "string[pyarrow]",
+        OdaSchema.BI_MULTI: "Int16[pyarrow]",
         OdaSchema.CATEGORY: "int16[pyarrow]",
         OdaSchema.USD_COMMITMENT: "float64[pyarrow]",
         OdaSchema.USD_DISBURSEMENT: "float64[pyarrow]",
@@ -78,9 +80,90 @@ def schema_types() -> dict:
         OdaSchema.CONVERGED_REPORTING: "string[pyarrow]",
         OdaSchema.COAL_FINANCING: "string[pyarrow]",
         OdaSchema.LEVEL: "string[pyarrow]",
+        OdaSchema.USD_COMMITMENT_DEFL: "float64[pyarrow]",
+        OdaSchema.USD_DISBURSEMENT_DEFL: "float64[pyarrow]",
+        OdaSchema.USD_RECEIVED_DEFL: "float64[pyarrow]",
+        OdaSchema.USD_ADJUSTMENT: "float64[pyarrow]",
+        OdaSchema.USD_ADJUSTMENT_DEFL: "float64[pyarrow]",
+        OdaSchema.USD_AMOUNT_UNTIED: "float64[pyarrow]",
+        OdaSchema.USD_AMOUNT_UNTIED_DEFL: "float64[pyarrow]",
+        OdaSchema.USD_AMOUNT_TIED: "float64[pyarrow]",
+        OdaSchema.USD_AMOUNT_TIED_DEFL: "float64[pyarrow]",
+        OdaSchema.USD_AMOUNT_PARTIAL_TIED: "float64[pyarrow]",
+        OdaSchema.USD_AMOUNT_PARTIAL_TIED_DEFL: "float64[pyarrow]",
+        OdaSchema.USD_IRTC_CODE: "string[pyarrow]",
+        OdaSchema.USD_EXPERT_COMMITMENT: "float64[pyarrow]",
+        OdaSchema.USD_EXPERT_EXTENDED: "float64[pyarrow]",
+        OdaSchema.USD_EXPORT_CREDIT: "float64[pyarrow]",
+        OdaSchema.COMMITMENT_NATIONAL: "float64[pyarrow]",
+        OdaSchema.DISBURSEMENT_NATIONAL: "float64[pyarrow]",
+        OdaSchema.GRANT_EQUIV_NATIONAL: "float64[pyarrow]",
+        OdaSchema.PARENT_CHANNEL_CODE: "int32[pyarrow]",
+        OdaSchema.LDC_FLAG_CODE: "int16[pyarrow]",
+        OdaSchema.LDC_FLAG: "string[pyarrow]",
+        OdaSchema.EXPECTED_START_DATE: "datetime64[ns]",
+        OdaSchema.COMPLETION_DATE: "datetime64[ns]",
+        OdaSchema.ENVIRONMENT: "int16[pyarrow]",
+        OdaSchema.DIG_CODE: "int16[pyarrow]",
+        OdaSchema.TRADE: "string[pyarrow]",
+        OdaSchema.RMNCH_CODE: "int16[pyarrow]",
+        OdaSchema.DRR_CODE: "int16[pyarrow]",
+        OdaSchema.NUTRITION: "int16[pyarrow]",
+        OdaSchema.DISABILITY: "int16[pyarrow]",
+        OdaSchema.FTC_CODE: "int16[pyarrow]",
+        OdaSchema.PBA_CODE: "int16[pyarrow]",
+        OdaSchema.INVESTMENT_PROJECT: "int16[pyarrow]",
+        OdaSchema.ASSOC_FINANCE: "int16[pyarrow]",
+        OdaSchema.BIODIVERSITY: "int16[pyarrow]",
+        OdaSchema.DESERTIFICATION: "int16[pyarrow]",
+        OdaSchema.COMMITMENT_DATE: "datetime64[ns]",
+        OdaSchema.TYPE_REPAYMENT: "int16[pyarrow]",
+        OdaSchema.NUMBER_REPAYMENT: "int16[pyarrow]",
+        OdaSchema.INTEREST1: "string[pyarrow]",
+        OdaSchema.INTEREST2: "string[pyarrow]",
+        OdaSchema.REPAYDATE1: "datetime64[ns]",
+        OdaSchema.REPAYDATE2: "datetime64[ns]",
+        OdaSchema.USD_INTEREST: "float64[pyarrow]",
+        OdaSchema.USD_OUTSTANDING: "float64[pyarrow]",
+        OdaSchema.USD_ARREARS_PRINCIPAL: "float64[pyarrow]",
+        OdaSchema.USD_ARREARS_INTEREST: "float64[pyarrow]",
+        OdaSchema.CAPITAL_EXPEND: "float64[pyarrow]",
+        OdaSchema.PSI_FLAG: "int16[pyarrow]",
+        OdaSchema.PSI_ADD_TYPE: "string[pyarrow]",
+        OdaSchema.PSI_ADD_ASSESS: "string[pyarrow]",
+        OdaSchema.PSI_ADD_DEV_OBJECTIVE: "string[pyarrow]",
     }
 
     return defaultdict(lambda: "string[pyarrow]", types)
+
+
+CATEGORICAL = [
+    OdaSchema.PROVIDER_NAME,
+    OdaSchema.AGENCY_NAME,
+    OdaSchema.RECIPIENT_NAME,
+    OdaSchema.RECIPIENT_REGION,
+    OdaSchema.RECIPIENT_INCOME,
+    OdaSchema.FLOW_NAME,
+    OdaSchema.PURPOSE_NAME,
+    OdaSchema.SECTOR_NAME,
+]
+
+
+def set_categorical_types(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Set the types of the columns in the dataframe.
+
+    Args:
+        df (pd.DataFrame): The input dataframe.
+
+    Returns:
+        pd.DataFrame: The dataframe with the types set.
+
+    """
+
+    converted_types = {c: "category" for c in df.columns if c in CATEGORICAL}
+
+    return df.astype(converted_types)
 
 
 def set_default_types(df: pd.DataFrame) -> pd.DataFrame:
