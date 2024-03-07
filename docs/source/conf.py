fcacb7afb3f4d3b5d6bdd235534c38e843cc2c61
Jorge Rivera <jorge.rivera@one.org>
2024-03-07 16:17:57 +0100

    Bump version: 1.1.4 → 1.1.5

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.abspath("../../oda_data/"))
 project = "oda_data"
 copyright = "2022, Jorge Rivera"
 author = "Jorge Rivera"
-release = "1.1.4"
+release = "1.1.5"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
