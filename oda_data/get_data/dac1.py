d56ec3992a478760599c836c0fdcbf5a9a597e84
Jorge Rivera <jorge.rivera@one.org>
2024-05-06 12:20:13 +0200

    implement oda_reader for dac1 and 2a

--- oda_data/get_data/dac1.py
+++ oda_data/get_data/dac1.py
@@ -1,19 +1,26 @@
+from oda_reader import download_dac1 as api_download_dac1
+
 from oda_data import config
-from oda_data.get_data import common
-from oda_data.logger import logger
 
 
-def download_dac1() -> None:
+def download_dac1(start_year: int | None = None, end_year: int | None = None) -> None:
     """Download the DAC1 file from OECD.Stat. Data for all years is downloaded at once.
     This function stores the raw data as a feather file in the raw data folder.
 
     Args:
-        small_version: optionally save a smaller version of the file with only key
-            columns (default is False)."""
-    logger.info(f"Downloading DAC1 data... This may take a while.")
-    common.download_single_table(
-        bulk_url=config.TABLE1_URL,
-        raw_file_name="Table1_Data.csv",
-        output_file_name="table1_raw",
-        save_path=config.OdaPATHS.raw_data,
-    )
+        start_year: optionally specify the start year for the data download.
+        end_year: optionally specify the end year for the data download.
+
+    """
+
+    suffix = ""
+
+    if start_year is not None:
+        suffix += f"_{start_year}"
+    if end_year is not None:
+        suffix += f"_{end_year}"
+
+    df = api_download_dac1(start_year=start_year, end_year=end_year)
+
+    # save the file
+    df.to_feather(config.OdaPATHS.raw_data / f"table1_raw{suffix}.feather")
