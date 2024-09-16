d65a4423d276fd83efe240c24f58d233be08f4b1
Jorge Rivera <jorge.rivera@one.org>
2024-09-16 16:07:34 +0200

    fix data reading

--- oda_data/get_data/multisystem.py
+++ oda_data/get_data/multisystem.py
@@ -1,7 +1,28 @@
 from oda_data import config
+from pathlib import Path
 
 from oda_data.get_data import common
 from oda_data.logger import logger
+
+
+def cloud_multisystem_download(
+    raw_file_name: str, output_file_name: str, save_path: Path
+) -> None:
+
+    # download the zip file from the website
+    file_content = common.get_zip(config.MULTISYSTEM_URL)
+
+    # Load the file into a DataFrame
+    df = common.read_zip_content(request_content=file_content, file_name=raw_file_name)
+
+    # Clean the DataFrame
+    df = common.clean_raw_df(df)
+
+    # save the file
+    df.to_feather(save_path / f"{output_file_name}.feather")
+
+    # log a message confirming the operation
+    logger.info(f"{raw_file_name} data downloaded and saved.")
 
 
 def download_multisystem() -> None:
@@ -13,13 +34,8 @@ def download_multisystem() -> None:
              columns (default is False)."""
 
     logger.info(f"Downloading Multisystem data... This may take a while.")
-    common.download_single_table(
-        bulk_url=config.MULTISYSTEM_URL,
+    cloud_multisystem_download(
         raw_file_name="MultiSystem entire dataset.txt",
         output_file_name="multisystem_raw",
         save_path=config.OdaPATHS.raw_data,
     )
-
-
-if __name__ == "__main__":
-    download_multisystem()
