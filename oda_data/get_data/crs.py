30685a3b2db184537d7e0ace72fddaf333036fc6
Jorge Rivera <jorge.rivera@one.org>
2025-02-01 16:38:19 -0600

    clean

--- oda_data/get_data/crs.py
+++ oda_data/get_data/crs.py
@@ -1,27 +1,11 @@
-from pandas.util._decorators import deprecate_kwarg
-from typing_extensions import deprecated
+from oda_reader import bulk_download_crs
 
 from oda_data import config
 from oda_data.clean_data.common import clean_raw_df
 from oda_data.logger import logger
 
 
-def save_clean_crs():
-    # Use oda_reader to download the full CRS data
-    from oda_reader import bulk_download_crs
-
-    df = bulk_download_crs()
-
-    # Clean the DataFrame
-    df = clean_raw_df(df)
-
-    df.to_parquet(config.OdaPATHS.raw_data / "fullCRS.parquet")
-    logger.info("Full CRS data downloaded successfully.")
-
-
-@deprecate_kwarg(old_arg_name="years", new_arg_name=None)
-@deprecate_kwarg(old_arg_name="small_version", new_arg_name=None)
-def download_crs(years: deprecated = None) -> None:
+def download_crs() -> None:
     """Download full CRS file.
 
     Args:
@@ -33,4 +17,11 @@ def download_crs(years: deprecated = None) -> None:
         "The package will now download the data, but it may take a while."
     )
 
-    save_clean_crs()
+    # Use oda_reader to download the full CRS data
+    df = bulk_download_crs()
+
+    # Clean the DataFrame
+    df = clean_raw_df(df)
+
+    df.to_parquet(config.OdaPATHS.raw_data / "fullCRS.parquet")
+    logger.info("Full CRS data downloaded successfully.")
