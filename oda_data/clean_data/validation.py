e8224df0921c5e1d66bd33c78b77ce92771e3be0
Jorge Rivera <jorge.rivera@one.org>
2025-02-06 10:19:11 +0100

    changes

--- oda_data/clean_data/validation.py
+++ oda_data/clean_data/validation.py
@@ -35,3 +35,22 @@ def validate_base_year(base_year: int | None, prices: str) -> None:
         raise ValueError(f"Base year can only be specified if prices are constant")
     if base_year is None and prices == "constant":
         raise ValueError(f"Base year must be specified for constant prices")
+
+
+def validate_input_parameters(
+    providers: list | int | None,
+    recipients: list | int | None,
+    currency: str,
+    measure: list | str,
+    prices: str,
+    base_year: int | None,
+) -> tuple:
+    """Validate the input parameters."""
+    providers = validate_providers(providers)
+    recipients = validate_recipients(recipients)
+    measure = validate_measure(measure)
+    validate_currency(currency)
+    validate_prices(prices)
+    validate_base_year(base_year=base_year, prices=prices)
+
+    return providers, recipients, measure
