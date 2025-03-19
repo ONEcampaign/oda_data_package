b7a07112c8cb698de15dc5f73fd093955a165325
Jorge Rivera <jorge.rivera@one.org>
2025-03-19 17:12:22 +0100

    cleaning

--- oda_data/__init__.py
+++ oda_data/__init__.py
@@ -1,7 +1,11 @@
-__version__ = "1.5.0"
+from importlib.metadata import version
+
+__version__ = version("oda_data")
 
 from oda_data import tools
 from oda_data.api.main import Indicators
+from oda_data.api.sources import Dac1Data, Dac2Data, MultiSystemData, CrsData
+from oda_data.tools.compatibility import ODAData
 from oda_data.tools.groupings import donor_groupings, recipient_groupings
 from oda_data.tools.sector_lists import add_sectors, add_broad_sectors
 
@@ -20,19 +24,14 @@ def set_data_path(path):
 
 __all__ = [
     "Indicators",
-    # "download_crs",
-    # "download_dac1",
-    # "download_dac2a",
-    # "download_multisystem",
-    # "read_crs",
-    # "read_dac1",
-    # "read_dac2a",
-    # "read_multisystem",
+    "Dac1Data",
+    "Dac2Data",
+    "MultiSystemData",
+    "CrsData",
     "donor_groupings",
     "recipient_groupings",
-    # "add_name",
     "add_sectors",
     "add_broad_sectors",
     "set_data_path",
-    "tools",
+    "ODAData"
 ]
