30f1429ee5c8b297deff735ab4155cdf7f426065
mharoruiz <88669820+mharoruiz@users.noreply.github.com>
2025-03-31 11:29:28 +0200

    Update dac1_functions.py

--- oda_data/indicators/dac1/dac1_functions.py
+++ oda_data/indicators/dac1/dac1_functions.py
@@ -6,7 +6,7 @@ def official_oda(data: pd.DataFrame) -> pd.DataFrame:
     Pre-2018, ODA is defined as aidtype_code 1010.
     Post-2018, ODA is defined as aidtype_code 11010.
     """
-    query = "(year < 2018 & aidtype_code == 1010) | (year >2018 & aidtype_code ==11010)"
+    query = "(year < 2018 & aidtype_code == 1010) | (year >= 2018 & aidtype_code ==11010)"
 
     data = data.query(query)
 
