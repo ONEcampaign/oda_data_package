59f70df27f5776a75524d2ed974f29be67b369af
Jorge Rivera <jorge.rivera@one.org>
2025-06-14 13:47:46 +0200

    Revert "set explicit paths"
    This reverts commit 4e969aabd2d78a294950e944572f9e05895c4206.

--- oda_data/tools/cache.py
+++ oda_data/tools/cache.py
@@ -2,16 +2,13 @@ import hashlib
 import json
 import time
 from pathlib import Path
-from typing import Optional
+from typing import Any, Dict, Optional
 
 import pandas as pd
 
-from oda_data import set_data_path
-from oda_data.config import ODAPaths
 from oda_data.logger import logger
 from oda_data.tools.names.create_mapping import snake_to_pascal
 
-set_data_path(ODAPaths.raw_data)
 
 def generate_param_hash(filters: list[tuple]) -> str:
     """Generates a reproducible hash from a list of tuples."""
